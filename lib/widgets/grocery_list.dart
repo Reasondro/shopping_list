2e27e8726a8bf57cb6a8593c1f1b438471158909
Reasondro <alessandrohasian16@gmail.com>
2024-03-29 23:24:34 +0700

    Finished for form and how to save them. Also add fallback content and dismissable for list tile

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
+// import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
 class GroceryList extends StatefulWidget {
@@ -10,42 +11,70 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void _addItem() {
-    Navigator.of(context).push(
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItem(),
       ),
     );
+
+    setState(() {
+      //* use setState here obviously, need to  update the list UI
+      if (newItem == null) {
+        return;
+      } else {
+        _groceryItems.add(newItem);
+      }
+    });
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Your Groceries"),
-        actions: [
-          IconButton(
-              onPressed: () {
-                //*could be a stateless widget if we pass Context to addItem
-                _addItem();
-              },
-              icon: const Icon(Icons.add))
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: groceryItems[index].category.color,
-          ),
-          trailing: Text(
-            (groceryItems[index].quantity).toString(),
+    Widget content = const Center(
+      child: Text("You have no items yet!"),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          key: ValueKey(_groceryItems[index].id),
+          onDismissed: (direction) => _removeItem(_groceryItems[index]),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              (_groceryItems[index].quantity).toString(),
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Your Groceries"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  //*could be a stateless widget if we pass Context to addItem
+                  _addItem();
+                },
+                icon: const Icon(Icons.add))
+          ],
+        ),
+        body: content);
   }
 }
